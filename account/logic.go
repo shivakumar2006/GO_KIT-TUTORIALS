d0d4494bee47eabeb144c87a8abb983fe10896dd
shivakumar2006 <official.shivakumar06@gmail.com>
2024-07-28 14:28:27 +0530

    second commit

---
+++ account/logic.go
@@ -0,0 +1,52 @@
+package account
+
+import (
+	"context"
+	"log"
+	// "github.com/gofrs/uuid"
+)
+
+type service struct {
+	repository Repository
+	logger     log.Logger
+}
+
+func NewService(rep Repository, logger log.Logger) Service {
+	return &service{
+		repository: rep,
+		logger: logger,
+	}
+}
+
+func (s service) CreateUser(ctx context.Context, Email string, Password string) (string, error) {
+	logger := log.With(s.logger, "method", "CreateUser")
+
+	uuid, _ := uuid.Newv4()
+	id := uuid.String()
+	user := User{
+		ID: id, 
+		Email: email,
+		Password: password,
+	}
+
+	if err := s.repository.CreateUser(ctx, user); err != nil {
+		level.Error(logger).log("err", err);
+		return "", err;
+
+		logger.Log("create user", id);
+
+		return "Success", nil;
+	}
+}
+
+func (s service) GetUser(ctx context.Context, id string) (string, error) {
+	logger := log.With(s.logger, "method", "GetUser")
+
+	email, err := s.repository.GetUser(ctx, id)
+
+	if err != nil {
+		level.Error(logger).log("err", err)
+		return "", err;
+	}
+	logger.Log("GetUser", id);
+}
